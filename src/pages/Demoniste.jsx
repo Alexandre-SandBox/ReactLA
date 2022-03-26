b25fefc77f3f4693e66d4ba329abf25f6f280733
Alexandre BREAULT <a.breault@iia-laval.fr>
2022-03-26 11:18:51 +0100

    test styled-components

--- src/pages/Demoniste.jsx
+++ src/pages/Demoniste.jsx
@@ -1,12 +1,14 @@
 import { useParams } from 'react-router-dom'
+import styled from 'styled-components'
 
+const CardLabel = styled.span``
 function Demoniste() {
     const { classChoice } = useParams()
     console.log(classChoice);
     return (
         <div>
             <h1>Questionnaire 🧮</h1>
-            <p>{ classChoice }</p>
+            <CardLabel>{ classChoice }</CardLabel>
         </div>
     )
 }
